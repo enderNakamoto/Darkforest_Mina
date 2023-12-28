6f37fa66bba7dd35746ff3ebacfa17c2b56202b9
enderNakamoto <ender.nakamoto@gmail.com>
2023-12-28 14:38:14 -0800

    incremental update to mine log

--- contracts/src/types/GlobalTypes.ts
+++ contracts/src/types/GlobalTypes.ts
@@ -1,22 +1,28 @@
-export type LocationId = string
-
-export interface WorldCoords {
+export interface Coordinate{
     x: number;
     y: number;
 }
 
-export interface Location {
-    coords: WorldCoords;
-    hash: LocationId;
-    perlin: number;
-}
 
-export interface ChunkFootprint {
-    bottomLeft: WorldCoords;
-    sideLength: number;
-}
+// export type LocationId = string
+
+// export interface WorldCoords {
+//     x: number;
+//     y: number;
+// }
+
+// export interface Location {
+//     coords: WorldCoords;
+//     hash: LocationId;
+//     perlin: number;
+// }
+
+// export interface ChunkFootprint {
+//     bottomLeft: WorldCoords;
+//     sideLength: number;
+// }
   
-export class ExploredChunkData {
-    chunkFootprint: ChunkFootprint;
-    planetLocations: Location[];
-}
+// export class ExploredChunkData {
+//     chunkFootprint: ChunkFootprint;
+//     planetLocations: Location[];
+// }
